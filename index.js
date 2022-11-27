c71cfaacfe63a7b9a495a53f909e50603b9a2154
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-27 14:33:48 +0600

    user name and pass added

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
-
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
     res.send('used products resale side is running');
 
 })
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yagkdpa.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.listen(port, () => {
